4c89af2ec1d4ca77c6a8229dcc012f274252c932
shejian <shejian.gg@gmail.com>
2018-06-12 23:21:29 +0800

    测试调用

--- app/src/main/java/com/avengers/power/MainActivity.kt
+++ app/src/main/java/com/avengers/power/MainActivity.kt
@@ -2,12 +2,17 @@ package com.avengers.power
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.avengers.zombiebase.ZombieBaseUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var zombieBaseUtils= ZombieBaseUtils()
+        zombieBaseUtils.getZombieLibraryInfo()
+
+
     }
 
 
