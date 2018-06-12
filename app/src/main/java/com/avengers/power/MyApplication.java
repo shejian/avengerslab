26073dbe6f9568be5f1c8325f08f56b6f41f1612
shejian <shejian@taiwu.com>
2018-06-12 13:45:56 +0800

    Squashed 'moonsub/' changes from c98c58e..c1e4663
    c1e4663 Merge commit 'dbedda7d5bc3e301c307f25fb2970e54a2c75b3d' into develop
    dbedda7 Squashed 'moonsub/' changes from 805b7dd..c98c58e
    1944e86 Merge commit 'deecec32072ff8cdf0f0e65f252310ddbdd23840' into develop
    58fc50b Merge commit 'deecec32072ff8cdf0f0e65f252310ddbdd23840' into develop
    deecec3 Squashed 'moonsub/' changes from 649ec5b..805b7dd
    44bce8f 删除moon子模块
    0fab0f8 删除moon子模块
    8535457 change ignore
    e46ed48 文件忽略规则
    46d91b0 加入moon子模块，使用subtree方式
    ed1404f Squashed 'moonsub/' content from commit 649ec5b
    cd9bfcc Merge commit 'ed1404fa95ae7cef307aeab2d30c7df8a0fd1c6c' as 'moonsub'
    682e31d 删除moon子模块
    2c3a257 关联moon子模块
    073f82a 删除子module
    f3cb399 删除子模块依赖
    0fa16c1 子模块依赖
    57f8ee7 子模块依赖
    9da874c 小调整
    604ad2a init
    8543b69 init project
    aa61689 Initial commit
    REVERT: c98c58e Merge commit 'deecec32072ff8cdf0f0e65f252310ddbdd23840' into develop
    REVERT: 805b7dd 独立项目的修改
    REVERT: edf8ed5 删除moon子模块
    REVERT: 6117132 删除moon子模块
    REVERT: 2bcdb6c change ignore
    REVERT: 1a43a1e 文件忽略规则
    REVERT: 90333f0 加入moon子模块，使用subtree方式
    REVERT: 649ec5b 测试修改提交
    REVERT: 05e86de 初始化项目
    REVERT: 0317e06 Create README.md
    
    git-subtree-dir: moonsub
    git-subtree-split: c1e466368abbbe62883b6e3390b17d272f7b2668

---
+++ app/src/main/java/com/avengers/power/MyApplication.java
@@ -0,0 +1,65 @@
+package com.avengers.power;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.multidex.MultiDex;
+
+import com.spinytech.macore.MaApplication;
+
+public class MyApplication extends MaApplication {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //注册全局的activity生命周期监听
+        registerActivityLifecycleCallbacks(new MyAppActivityLifecycleCallbacks());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void initializeAllProcessRouter() {
+
+    }
+
+    @Override
+    protected void initializeLogic() {
+
+    }
+
+    @Override
+    public boolean needMultipleProcess() {
+        return false;
+    }
+
+    /**
+     * 当应用程序被终止时将被调用，但是被系统干掉的情况除外
+     */
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    /**
+     * 配置改变时调用
+     *
+     * @param newConfig
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * 当后台程序已经被终止时且资源还匮乏时会调用这个方法，可以做一些释放的工作
+     */
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+}
